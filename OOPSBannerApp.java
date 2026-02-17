2d96cd76efbdde8d0436748b778960b350fa4ff6
Vedant-S-Hada <vh8862@srmist.edu.in>
2026-02-17 10:45:10 +0530

    UC2: Print OOPS Banner using Print Statements

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -12,9 +12,6 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        // Each letter is 7 lines tall and width is 9 characters.
-        // We print O O P S side-by-side using string concatenation.
-
         System.out.println("   ***   " + "   ***   " + "  *****  " + "  *****  ");
         System.out.println(" **   ** " + " **   ** " + " **   ** " + " **      ");
         System.out.println("**     **" + "**     **" + " **   ** " + " **      ");
