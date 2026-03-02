32acbaca2b96d2983bfe43a29503845fe5c6b3b0
Vedant-S-Hada <vh8862@srmist.edu.in>
2026-03-02 09:29:53 +0530

    UC6 - Banner App using Static Methods

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,22 +1,61 @@
-// UC5 Implementation
 public class OOPSBannerApp {
 
+    // Method to generate pattern for 'O'
+    public static String[] getOPattern() {
+        return new String[]{
+                "  ***  ",
+                " *   * ",
+                "*     *",
+                "*     *",
+                "*     *",
+                " *   * ",
+                "  ***  "
+        };
+    }
+
+    // Method to generate pattern for 'P'
+    public static String[] getPPattern() {
+        return new String[]{
+                " ***** ",
+                " *   * ",
+                " *   * ",
+                " ***** ",
+                " *     ",
+                " *     ",
+                " *     "
+        };
+    }
+
+    // Method to generate pattern for 'S'
+    public static String[] getSPattern() {
+        return new String[]{
+                "  *****",
+                " *     ",
+                " *     ",
+                "  **** ",
+                "      *",
+                "      *",
+                " ***** "
+        };
+    }
+
+    // Main method
     public static void main(String[] args) {
 
-        // Step 1: Declare + Initialize array in one step
-        String[] lines = {
-            String.join(" ", " *** ", " *** ", " ***** ", " ***** "),
-            String.join(" ", "*   *", "*   *", "*     ", "*     "),
-            String.join(" ", "*   *", "*   *", "*     ", "*     "),
-            String.join(" ", "*   *", "*   *", " *****", " *****"),
-            String.join(" ", "*   *", "*   *", "     *", "     *"),
-            String.join(" ", "*   *", "*   *", "     *", "     *"),
-            String.join(" ", " *** ", " *** ", " *****", " *****")
-        };
+        // Call methods to get patterns
+        String[] oPattern1 = getOPattern();
+        String[] oPattern2 = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
 
-        // Step 2: Loop to print
-        for (String line : lines) {
-            System.out.println(line);
+        // Loop to print banner line by line
+        for (int i = 0; i < oPattern1.length; i++) {
+            System.out.println(
+                    oPattern1[i] + "  " +
+                    oPattern2[i] + "  " +
+                    pPattern[i] + "  " +
+                    sPattern[i]
+            );
         }
     }
 }
