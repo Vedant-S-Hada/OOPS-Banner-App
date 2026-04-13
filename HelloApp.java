62515bbe25995b93022895b7cdb9c775e6d44fd3
Vedant-S-Hada <vh8862@srmist.edu.in>
2026-04-13 10:35:03 +0530

    final commit

--- HelloApp.java
+++ HelloApp.java
@@ -1,64 +1,15 @@
-import java.util.HashMap;
-import java.util.Map;
-
 public class HelloApp {
 
     public static void main(String[] args) {
 
-        Map<Character, String[]> patternMap = new HashMap<>();
+        // Default name
+        String name = "World";
 
-        patternMap.put('O', new String[]{
-                " *** ",
-                "*   *",
-                "*   *",
-                "*   *",
-                " *** "
-        });
-
-        patternMap.put('P', new String[]{
-                "**** ",
-                "*   *",
-                "**** ",
-                "*    ",
-                "*    "
-        });
-
-        patternMap.put('S', new String[]{
-                " ****",
-                "*    ",
-                " *** ",
-                "    *",
-                "**** "
-        });
-
-        renderBanner("OOPS", patternMap);
-    }
-
-    public static void renderBanner(String word, Map<Character, String[]> map) {
-
-        int height = 5;
-
-        StringBuilder[] banner = new StringBuilder[height];
-
-        for (int i = 0; i < height; i++) {
-            banner[i] = new StringBuilder();
+        // Check if a name is provided as a command-line argument
+        if (args.length > 0) {
+            name = args[0]; // Use the provided name
         }
 
-        for (char c : word.toCharArray()) {
-
-            String[] pattern = map.get(c);
-
-            if (pattern != null) {
-
-                for (int i = 0; i < height; i++) {
-                    banner[i].append(pattern[i]).append(" ");
-                }
-
-            }
-        }
-
-        for (StringBuilder line : banner) {
-            System.out.println(line);
-        }
+        System.out.println("Hello, " + name + "!");
     }
 }
